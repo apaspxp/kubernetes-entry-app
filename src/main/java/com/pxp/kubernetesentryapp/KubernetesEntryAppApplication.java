273118920e9e67045492cbd0601240ff22575a6b
apaspxp <63920754+apaspxp@users.noreply.github.com>
2021-06-12 18:08:10 +0530

    Update KubernetesEntryAppApplication.java

--- src/main/java/com/pxp/kubernetesentryapp/KubernetesEntryAppApplication.java
+++ src/main/java/com/pxp/kubernetesentryapp/KubernetesEntryAppApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class KubernetesEntryAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(KubernetesEntryAppApplication.class, args);
 	}
 
